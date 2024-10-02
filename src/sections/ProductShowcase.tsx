03793dc3336a8e3e13660a79570870cf5a8267b2
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-02 19:11:11 -0400

    added some framer motion animation

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,24 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
 import Image from "next/image";
-
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to -[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to -[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="max-w-[540px] mx-auto">
           <div className="flex justify-center">
@@ -21,18 +34,25 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="product image" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="Pyramid Image"
             height={252}
             width={252}
             className="hidden md:block absolute -right-32 -top-32 "
+            style={{
+              translateY: translateY,
+            }}
           />
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="Tube image"
             className="hidden md:block absolute bottom-24 -left-36"
             height={248}
+            width={248}
+            style={{
+              translateY: translateY,
+            }}
           />
         </div>
       </div>
