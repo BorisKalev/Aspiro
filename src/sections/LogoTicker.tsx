6dd2b8dfc11d8a19e87ec3a8722cb53b158cb908
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-10-02 14:56:49 -0400

    added the product showcase section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,34 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image className={"logo-ticker"} src={acmeLogo} alt="Acme Logo" />
+            <Image
+              className={"logo-ticker"}
+              src={quantumLogo}
+              alt="Quantum Logo"
+            />
+            <Image className={"logo-ticker"} src={echoLogo} alt="Echo Logo" />
+            <Image
+              className={"logo-ticker"}
+              src={celestialLogo}
+              alt="Celestial Logo"
+            />
+            <Image className={"logo-ticker"} src={pulseLogo} alt="Pulse Logo" />
+            <Image className={"logo-ticker"} src={apexLogo} alt="Apex Logo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
